dd26146b578693651e7d56bb9df9e495853c791e
bengoodheart <bgoodheart@fordham.edu>
2023-04-16 19:35:28 -0400

    good lord

--- index.js
+++ index.js
@@ -4,32 +4,33 @@ const fs = require('fs')
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const edited = "/output"
 
-const rawImage = fs.readFile("/input-src/test.jpg", (()=>{
-    console.log("image loaded")
-}))
 
-httpServer.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type' : 'image/jpeg'})
-    res.end(rawImage)
-}).listen(port, hostname)
-console.log(`Server running at ${hostname}:${port}`)
 
-//OPEN UP THE RAW FILE
 
-/**
-fs.readFile("./input-src/test.jpg", function (err, data){
+function serveOriginalImage(sourcePath){
+    fs.readFile(sourcePath, function (err, data){
         if (err) throw err;
         httpServer.createServer(function(req, res){
-            res.writeHead(200, {'Content-Type' : 'text/html'})
-            res.write('<html><body><img src="data:image/jpeg;base64,')
-            res.write(Buffer.from(data).toString('base64'));
-            res.end('"/></body></html>');
+            res.writeHead(200, {'Content-Type' : 'image/jpeg'})
+            res.end(data)
         }).listen(port, hostname)
         console.log(`Server running at ${hostname}:${port}`)
     })
+}
 
-    */
+function createOutputDOM(){
+    let endpoint = port + edited
+    const outputServer = httpServer.createServer(function(req, res){
+        res.writeHead(200, {'Content-Type' : 'text/html'})
+        res.write("Output")
+        res.end()
+    }).listen(3001, hostname)
+    console.log("Output being served on " + `${hostname}:3001`)
+}
 
-
+const srcPath = "./input-src/test.jpg"
+createOutputDOM()
+serveOriginalImage(srcPath)
 
