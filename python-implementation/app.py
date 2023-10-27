1aff5f7fed3d5879c250d5539b3b6e31d9c84430
Benjamin <bgoodheart@fordham.edu>
2023-10-27 09:52:12 -0400

    moss

--- python-implementation/app.py
+++ python-implementation/app.py
@@ -3,8 +3,9 @@ from PIL import Image, ImageEnhance
 
 #TODO: ADD CLI argument input
 
-im = Image.open("../input-src/v1-test.jpg")
+im = Image.open("../input-src/moss-text.jpg")
 im = im.reduce(3)
-im = ImageEnhance.Color(im).enhance(5)
-im = ImageEnhance.Brightness(im).enhance(.6)
-im.save("../input-src/v1-test-deg.jpg", format=None, quality=5)
+im = ImageEnhance.Color(im).enhance(1.5)
+im = ImageEnhance.Brightness(im).enhance(1)
+im = ImageEnhance.Sharpness(im).enhance(2)
+im.save("../input-src/moss-text-deg-3.jpg", format=None, quality=5)
